8de17ef95bae88c44319dd9b415f5e8c36160e0a
trinhminhduc9x <duc1997th@gmail.com>
2022-10-12 16:34:45 +0700

    add

---
+++ TestPhanMemThu/src/main/java/com/codegym/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.codegym.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping(value = "/hello")
+    public String showHello(){
+        return "/hello";
+    }
+}
